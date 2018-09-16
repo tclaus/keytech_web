5cb8936fb185f4b42aca20be429f10ae689d888a
Thorsten Claus <thorstenclaus@web.de>
2018-09-16 12:18:04 +0200

    require keytechKit with lower 'k'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
-require 'KeytechKit'
+require 'keytechKit'
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
