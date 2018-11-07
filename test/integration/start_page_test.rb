bd476933123938ae2ee0837bac1f3c6ab5db9fbf
Thorsten Claus <thorstenclaus@web.de>
2018-11-07 07:46:30 +0100

    added simple login test

--- test/integration/start_page_test.rb
+++ test/integration/start_page_test.rb
@@ -1,15 +1,23 @@
 require 'test_helper'
 
 class StartPageTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
 
-  test "can see the welcome page" do
-    get "/"
-    assert_select "h1", "PLM Web"
+  test 'can see the welcome page' do
+    get '/'
+    assert_select 'h1', 'PLM Web'
   end
 
-  test "can see the login page" do
-    get "/login"
-    assert_select "h1", "PLM Web"
+  test 'can see the login page' do
+    get '/login'
+    assert_select 'h1', 'PLM Web'
   end
+
   # Extend fpr static pages
+  test 'can login' do
+    sign_in users(:host)
+    get '/'
+    # Assert that the navbar shows the name of loggen in user
+    assert_select '#navbarDropdown', users(:host).email
+  end
 end
